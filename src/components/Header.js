cfe9f76d414beaf49c426483d07931153020a7ec
Ali <aliahmedtaha7077@gmail.com>
2024-10-04 08:49:37 +0400

    Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,30 +12,30 @@ function Header() {
 
   return (
     <section id="Navbar" className='navbar--main'>
-      <h1 className='Logo'>at</h1>
-      <img alt="" className='nav--bars' src={BARS} onClick={toggleMenu} />
-      <ul>
-        <li>
-          <Link to="Home" smooth={true} duration={1000} offset={-100}>
-            Home
+        <h1 className='Logo'>at</h1>
+        <img alt="" className='nav--bars' src={BARS} onClick={toggleMenu} />
+        <div className={`ul ${isMenuOpen ? 'open' : ''}`}>
+          <li>
+              <Link to="Home" smooth={true} duration={1000} offset={-100}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="About" smooth={true} duration={1000} offset={-50}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="Service" smooth={true} duration={1000} offset={-100}>
+                Services
+              </Link>
+            </li>
+        </div>
+        <button className='navbar--button'>
+          <Link to="Contact" smooth={true} duration={1000}>
+            Contact Me
           </Link>
-        </li>
-        <li>
-          <Link to="About" smooth={true} duration={1000} offset={-50}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="Service" smooth={true} duration={1000} offset={-100}>
-            Services
-          </Link>
-        </li>
-      </ul>
-      <button className='navbar--button'>
-        <Link to="Contact" smooth={true} duration={1000}>
-          Contact Me
-        </Link>
-      </button>
+        </button>
     </section>
   );
 }
